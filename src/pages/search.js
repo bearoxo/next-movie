cd00a4a3bf3316240cbec18943742119e04ccd55
Andy Fazulus <a.y.banzai@gmail.com>
2023-07-27 09:52:34 +0800

    Add layout for main

---
+++ src/pages/search.js
@@ -0,0 +1,11 @@
+import HeroSearch from '../components/hero-search';
+import SearchResult from '../components/search-result';
+
+export default function Search() {
+  return (
+    <>
+      <HeroSearch />
+      <SearchResult />
+    </>
+  )
+}
