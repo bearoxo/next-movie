a881c3ea3bba289c9ca8806d4f698a739aed0c19
Andy Fazulus <a.y.banzai@gmail.com>
2023-07-30 06:52:48 +0800

    Add movie context and card base layout.

--- src/assets/components/last-watch.js
+++ src/assets/components/last-watch.js
@@ -1,24 +1,49 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useContext } from 'react';
+import { MovieContext } from '../contexts/MovieContext'
 import { useRouter } from 'next/router';
 import styles from '../styles/components/last-watch.module.css';
 
-export default function LastWatch () {
-  const router = useRouter();
+import duration from '../../../public/img/duration.svg';
+import views from '../../../public/img/views.svg'
 
+export default function LastWatch () {
+  const { movies } = useContext(MovieContext)  
+  console.log("2...")
+  movies.map((movie) => {
+    console.log(movie)
+  })
+  
   return (
-    <div className={styles.lastWatch}>
-      <div className={styles.title}>
-        <h3>New Releases</h3>
-        <h5>View more</h5>
+      <div className={styles.lastWatch}>
+        <div className={styles.title}>
+          <h3>New Releases</h3>
+          <h5>View more</h5>
+        </div>
+        <div className={styles.cardContainer}>
+          {movies.map((movie) => (
+            <div key={movie.Movie_ID} className={styles.cards}>
+              <div className={styles.poster}>
+                <Image src={movie.Poster} alt={movie.Title} loading="lazy" layout="fill" objectFit="cover" objectPosition="center" />
+              </div>
+              <div className={styles.overlay}>
+                <div className={styles.genre}>{movie.Genre}</div>
+                <div className={styles.details}>
+                  <div className={styles.duration}>
+                    <Image src={duration} alt="Views" height={24} width={24} />
+                    {movie.Duration}
+                  </div>
+                  <div className={styles.views}>
+                    <Image src={views} alt="Views" height={24} width={24} />
+                    {movie.Views}
+                  </div>
+                </div>
+                <div className={styles.cardTitle}><h3>{movie.Title}</h3></div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className={styles.cardContainer}>
-        <div className={styles.cards}></div>
-        <div className={styles.cards}></div>
-        <div className={styles.cards}></div>
-        <div className={styles.cards}></div>
-        <div className={styles.cards}></div>
-      </div>
-    </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import Hero from '../assets/components/hero';
 import LastWatch from '../assets/components/last-watch'
+import { MovieProvider } from '../assets/contexts/MovieContext';
 
 export default function Home() {
   return (
     <>
+    <MovieProvider>
       <Hero />
       <LastWatch />
+    </MovieProvider>
     </>
   )
 }
