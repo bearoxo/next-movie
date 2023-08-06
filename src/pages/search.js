5634f68459917f5d29185ea3b28ada48881bfdd8
Andy Fazulus <a.y.banzai@gmail.com>
2023-08-06 08:06:50 +0800

    Add search-result.

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,11 +1,14 @@
 import HeroSearch from '../assets/components/hero-search';
-// import SearchResult from '../assets/components/search-result';
+import SearchResult from '../assets/components/search-result';
+import { SearchProvider } from '../assets/contexts/SearchContext';
 
 export default function Search() {
   return (
     <>
-      <HeroSearch />
-      {/* <SearchResult /> */}
+      <SearchProvider>
+        <HeroSearch />
+        <SearchResult />
+      </SearchProvider>
     </>
   )
 }
